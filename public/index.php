b0895140d58512a48b1e904f184627deaad283e1
PXgamer <owzie123@gmail.com>
2017-06-28 10:11:58 +0100

    Changed config location

--- public/index.php
+++ public/index.php
@@ -5,8 +5,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $RSSFeed = new ToRSS\RSSFeed();
 
+$configLocation = __DIR__ . '/../config/shows.json';
+
 // Get shows
-$file = json_decode(file_get_contents("inc/shows.json"), true);
+$file = json_decode(file_get_contents($configLocation), true);
 
 header('Content-type: text/xml');
 
